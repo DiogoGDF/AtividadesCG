8ff52b21427ae4a0efbd9140d2f88444703a36cc
Diogo Garbinato <diogogarbinato@gmail.com>
2024-07-02 16:23:51 -0300

    feat: cubo obj

---
+++ Atividade Vivencial M2/Exericio8/Origem.cpp
@@ -0,0 +1,425 @@
+// Aluno: Diogo Garbinato de Fagundes       Matrícula: 1189650
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+// GLAD
+#include <glad/glad.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Protótipo da função de callback de teclado
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Protótipos das funções
+int setupShader();
+int setupGeometry();
+//int lerOBJ(string filepath, int& nVerts, glm::vec3 color = glm::vec3(1.0, 0.0, 1.0));
+int lerOBJ(string filepath, int& nVerts);
+
+// Dimensões da janela (pode ser alterado em tempo de execução)
+const GLuint WIDTH = 1000, HEIGHT = 1000;
+
+// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
+const GLchar* vertexShaderSource = "#version 450\n"
+"layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 color;\n"
+"uniform mat4 model;\n"
+"out vec4 finalColor;\n"
+"void main()\n"
+"{\n"
+"gl_Position = model * vec4(position, 1.0);\n"
+"finalColor = vec4(color, 1.0);\n"
+"}\0";
+
+// Código fonte do Fragment Shader (em GLSL): ainda hardcoded
+const GLchar* fragmentShaderSource = "#version 450\n"
+"in vec4 finalColor;\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = finalColor;\n"
+"}\n\0";
+
+// Variáveis globais para armazenar as transformações
+bool rotateX = false, rotateY = false, rotateZ = false;
+float transX = 0.0f, transY = 0.0f, transZ = 0.0f;
+float scale = 1.0f;
+
+
+// Função MAIN
+int main()
+{
+    // Inicialização da GLFW
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Criação da janela GLFW
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola 3D -- Diogo!", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    // Fazendo o registro da função de callback para a janela GLFW
+    glfwSetKeyCallback(window, key_callback);
+
+    // GLAD: carrega todos os ponteiros de funções da OpenGL
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Compilando e ativando o shader
+    int shaderProgram = setupShader();
+    glUseProgram(shaderProgram);
+
+    // Definindo a geometria
+    int VAO = setupGeometry();
+
+    // Carregamento do cubo OBJ
+    int nVerts;
+    GLuint cubeVAO = lerOBJ("../../3D_models/Cube/cube.obj", nVerts);
+
+    // Define a matriz de projeção
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+    GLint projLoc = glGetUniformLocation(shaderProgram, "projection");
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+    // Habilitar o teste de profundidade
+    glEnable(GL_DEPTH_TEST);
+
+    // Loop da aplicação - "game loop"
+    while (!glfwWindowShouldClose(window))
+    {
+        // Verifica se há eventos de entrada (como pressionar teclas, mover o mouse, etc.)
+        glfwPollEvents();
+
+        // Renderiza o frame
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Define a matriz de visualização
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+        GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        // Define a matriz de modelo
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(transX, transY, transZ));
+        model = glm::scale(model, glm::vec3(scale, scale, scale));
+        if (rotateX)
+            model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        if (rotateY)
+            model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        if (rotateZ)
+            model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        // Renderiza o cubo
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, nVerts);
+        glBindVertexArray(0);
+
+        // Troca os buffers e desenha
+        glfwSwapBuffers(window);
+    }
+
+    // Libera os recursos alocados pela GLFW
+    glfwTerminate();
+    return 0;
+}
+
+int setupShader()
+{
+    // Compilando o Vertex Shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Checando erros de compilação do Vertex Shader
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+    }
+
+    // Compilando o Fragment Shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Checando erros de compilação do Fragment Shader
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+    }
+
+    // Linkando os shaders em um programa
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Checando erros de linkagem
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
+    }
+
+    // Deletando os shaders após terem sido linkados
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+int setupGeometry()
+{
+    // Definindo os vértices e cores para o cubo
+    GLfloat vertices[] = {
+        // Frente
+        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+
+        // Trás
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+
+        // Esquerda
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+
+        // Direita
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+
+        // Embaixo
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+
+        // Em cima
+        -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f
+    };
+
+    GLuint VBO, VAO;
+
+    // Geração do identificador do VBO
+    glGenBuffers(1, &VBO);
+
+    // Faz a conexão (vincula) do buffer como um buffer de array
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Envia os dados do array de floats para o buffer da OpenGl
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Geração do identificador do VAO (Vertex Array Object)
+    glGenVertexArrays(1, &VAO);
+
+    // Vincula (bind) o VAO primeiro, e em seguida conecta e seta o(s) buffer(s) de vértices
+    // e os ponteiros para os atributos
+    glBindVertexArray(VAO);
+
+    // Para cada atributo do vértice, criamos um "AttribPointer" (ponteiro para o atributo), indicando:
+    // Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+    // Número de valores que o atributo tem (por exemplo, 3 coordenadas xyz)
+    // Tipo do dado
+    // Se está normalizado (entre zero e um)
+    // Tamanho em bytes
+    // Deslocamento a partir do byte zero
+
+    // Atributo posição (x, y, z)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    // Atributo cor (r, g, b)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    // Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice
+    // atualmente vinculado - para que depois possamos desvincular com segurança
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+    glBindVertexArray(0);
+
+    return VAO;
+}
+
+// Função de callback de teclado
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
+    if (key == GLFW_KEY_X && action == GLFW_PRESS)
+        rotateX = !rotateX;
+
+    if (key == GLFW_KEY_Y && action == GLFW_PRESS)
+        rotateY = !rotateY;
+
+    if (key == GLFW_KEY_Z && action == GLFW_PRESS)
+        rotateZ = !rotateZ;
+
+    // Controles para mover o cubo nos eixos x, y e z
+    if (key == GLFW_KEY_W && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        transY += 0.01f;
+
+    if (key == GLFW_KEY_S && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        transY -= 0.01f;
+
+    if (key == GLFW_KEY_A && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        transX -= 0.01f;
+
+    if (key == GLFW_KEY_D && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        transX += 0.01f;
+
+    if (key == GLFW_KEY_I && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        transZ += 0.01f;
+
+    if (key == GLFW_KEY_J && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        transZ -= 0.01f;
+
+    // Controles para escalar o cubo
+    if (key == GLFW_KEY_LEFT_BRACKET && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        scale -= 0.1f;
+
+    if (key == GLFW_KEY_RIGHT_BRACKET && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        scale += 0.1f;
+}
+
+int lerOBJ(string filepath, int& nVerts) {
+    ifstream fileStream(filepath, ios::in);
+    if (!fileStream.is_open()) {
+        cerr << "Não foi possível abrir o arquivo OBJ!" << endl;
+        return -1;
+    }
+
+    vector<glm::vec3> vertices;
+    vector<glm::vec2> texCoords;
+    vector<glm::vec3> normals;
+    vector<int> indices;
+    vector<GLfloat> vertexBuffer;
+    string line;
+
+    while (getline(fileStream, line)) {
+        stringstream ss(line);
+        string prefix;
+        ss >> prefix;
+
+        if (prefix == "v") {
+            glm::vec3 vertex;
+            ss >> vertex.x >> vertex.y >> vertex.z;
+            vertices.push_back(vertex);
+        }
+        else if (prefix == "vt") {
+            glm::vec2 texCoord;
+            ss >> texCoord.s >> texCoord.t;
+            texCoords.push_back(texCoord);
+        }
+        else if (prefix == "vn") {
+            glm::vec3 normal;
+            ss >> normal.x >> normal.y >> normal.z;
+            normals.push_back(normal);
+        }
+        else if (prefix == "f") {
+            string vertexData[3];
+            ss >> vertexData[0] >> vertexData[1] >> vertexData[2];
+
+            for (int i = 0; i < 3; i++) {
+                size_t pos = vertexData[i].find("/");
+                int vIndex = atoi(vertexData[i].substr(0, pos).c_str()) - 1;
+                indices.push_back(vIndex);
+
+                vertexBuffer.push_back(vertices[vIndex].x);
+                vertexBuffer.push_back(vertices[vIndex].y);
+                vertexBuffer.push_back(vertices[vIndex].z);
+
+                vertexData[i] = vertexData[i].substr(pos + 1);
+                pos = vertexData[i].find("/");
+                int vtIndex = atoi(vertexData[i].substr(0, pos).c_str()) - 1;
+                vertexBuffer.push_back(texCoords[vtIndex].s);
+                vertexBuffer.push_back(texCoords[vtIndex].t);
+
+                vertexData[i] = vertexData[i].substr(pos + 1);
+                int vnIndex = atoi(vertexData[i].c_str()) - 1;
+                vertexBuffer.push_back(normals[vnIndex].x);
+                vertexBuffer.push_back(normals[vnIndex].y);
+                vertexBuffer.push_back(normals[vnIndex].z);
+            }
+        }
+    }
+
+    fileStream.close();
+
+    GLuint VBO, VAO;
+
+    nVerts = vertexBuffer.size() / 8;
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertexBuffer.size() * sizeof(GLfloat), vertexBuffer.data(), GL_STATIC_DRAW);
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(5 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    return VAO;
+}
+
